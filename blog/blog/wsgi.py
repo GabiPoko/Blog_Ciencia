26526fd234d1210f20818c2f4eca1e37c6dbbaae
GabiPoko <gabipokorasky@gmail.com>
2025-02-15 19:13:26 -0300

    Actualización wgsi.py

--- blog/blog/wsgi.py
+++ blog/blog/wsgi.py
@@ -8,15 +8,9 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-import sys
-
-path = os.path.expanduser('~/proyectoblogdjango')
-if path not in sys.path:
-    sys.path.append(path)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'proyectoblogdjango.base'
 
 from django.core.wsgi import get_wsgi_application
-from django.contrib.staticfiles.handlers import StaticFilesHandler
 
-application = StaticFilesHandler(get_wsgi_application())
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
+
+application = get_wsgi_application()
