6a55c317cdcd43a781e631a6018fa85aca659d10
GabiPoko <gabipokorasky@gmail.com>
2025-02-15 18:46:51 -0300

    wsgi.py modificado

--- blog/blog/wsgi.py
+++ blog/blog/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = os.path.expanduser('~/proyectoblogdjango')
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'proyectoblogdjango.base'
 
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
-
-application = get_wsgi_application()
+application = StaticFilesHandler(get_wsgi_application())
